64068efdf8ee017d4c5a9c7f5e60045a573d2c96
John Doe <johndoe@example.com>
2022-08-31 17:37:57 +0800

    feat: Add TypeScript

--- scripts/install-nodejs.sh
+++ scripts/install-nodejs.sh
@@ -6,6 +6,7 @@ INSTALLDIR="/opt/nodejs-$VERSION"
 set -xe
 
 curl -fSsL "https://nodejs.org/dist/v$VERSION/node-v$VERSION-linux-x64.tar.xz" -o /tmp/nodejs-$VERSION.tar.gz
-sudo mkdir -p $INSTALLDIR
-sudo tar -xf /tmp/nodejs-$VERSION.tar.gz -C $INSTALLDIR --strip-components=1
+mkdir -p $INSTALLDIR && tar -xf /tmp/nodejs-$VERSION.tar.gz -C $INSTALLDIR --strip-components=1
 rm /tmp/nodejs-$VERSION.tar.gz
+
+npm install --prefix $INSTALLDIR -g typescript@4.8.2
