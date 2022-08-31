8803ac8fd75532234272e9ced81788aaee0da6eb
John Doe <johndoe@example.com>
2022-08-31 13:44:31 +0800

    chore: Add OpenJDK install script

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -5,7 +5,7 @@ INSTALLDIR="/opt/go-$VERSION"
 
 set -xe
 
-mkdir $INSTALLDIR
+mkdir -p $INSTALLDIR
 curl -fSsL "https://go.dev/dl/go$VERSION.linux-amd64.tar.gz" -o /tmp/go-$VERSION.tar.gz
 tar -xf /tmp/go-$VERSION.tar.gz -C $INSTALLDIR --strip-components=1
 rm /tmp/go-$VERSION.tar.gz
