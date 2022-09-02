466b559806401531c2befdac4cda440a6a28d364
John Doe <johndoe@example.com>
2022-09-02 11:23:55 +0800

    chore: Update install-openjsdk script

--- scripts/install-openjdk.sh
+++ scripts/install-openjdk.sh
@@ -2,10 +2,11 @@
 
 VERSION="18"
 INSTALLDIR="/opt/openjdk-$VERSION"
+DOWNLOADURI="https://download.java.net/java/GA/jdk18.0.2.1/db379da656dc47308e138f21b33976fa/1/GPL/openjdk-18.0.2.1_linux-x64_bin.tar.gz"
 
 set -xe
 
-curl -fSsL "https://download.java.net/java/GA/jdk18.0.2.1/db379da656dc47308e138f21b33976fa/1/GPL/openjdk-18.0.2.1_linux-x64_bin.tar.gz" -o /tmp/openjdk-$VERSION.tar.gz
+curl -fSsL $DOWNLOADURI -o /tmp/openjdk-$VERSION.tar.gz
 sudo mkdir -p $INSTALLDIR
 sudo tar -xf /tmp/openjdk-$VERSION.tar.gz -C $INSTALLDIR --strip-components=1
 rm /tmp/openjdk-$VERSION.tar.gz
