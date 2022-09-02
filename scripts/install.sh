08f6b5ac2295bd28484a18542242db3a6456fe85
John Doe <johndoe@example.com>
2022-09-03 01:17:45 +0800

    chore: Add dockerbuild & install scripts

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+REPO_ROOT="$( cd -- "$(dirname "$0")/.." >/dev/null 2>&1 || exit; pwd -P )"
+
+for langdir in "$REPO_ROOT"/src/embed/lang-* ; do
+  langname=$(basename "$langdir")
+  langname=${langname#"lang-"}
+  if [ "$1" == "all" ] || [ "$1" == "$langname" ]; then
+    (set -xe; cd "$langdir"; [ -f ./install.sh ]; sudo sh ./install.sh)
+  fi
+done
