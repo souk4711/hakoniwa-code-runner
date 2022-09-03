773b4a6ba87521d25934afa5d8da9741e1b010d4
John Doe <johndoe@example.com>
2022-09-03 12:34:11 +0800

    chore: Update rust install script

--- src/embed/lang-rust/install.sh
+++ src/embed/lang-rust/install.sh
@@ -9,3 +9,4 @@ curl --proto '=https' --tlsv1.2 -fSsL https://sh.rustup.rs -o /tmp/rustup-init.s
 HOME=$INSTALLDIR \
   sh /tmp/rustup-init.sh -v -y --no-modify-path --default-toolchain=$VERSION
 rm /tmp/rustup-init.sh
+rm -rf $INSTALLDIR/.rustup/toolchains/$VERSION-*/share
