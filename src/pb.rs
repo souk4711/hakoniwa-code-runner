c80f8392e3f56579b9db254ef9ef301b8cc6f014
John Doe <johndoe@example.com>
2023-08-28 13:38:36 +0800

    chore: Add github workflow

--- src/pb.rs
+++ src/pb.rs
@@ -1,10 +1,12 @@
 pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("hakoniwa-code-runner");
 
 pub mod languages {
+    #![allow(clippy::all)]
     tonic::include_proto!("languages");
 }
 
 pub mod runs {
+    #![allow(clippy::all)]
     tonic::include_proto!("runs");
 
     impl From<std::time::Duration> for Duration {
