7b0c60059afd8445f2bb75878901579c9aca5238
John Doe <johndoe@example.com>
2024-02-18 20:20:51 +0800

    fix: Cabal update appears to hang

--- .devcontainer/lang-haskell/install.sh
+++ .devcontainer/lang-haskell/install.sh
@@ -6,6 +6,7 @@ INSTALLDIR="/opt/haskell-$VERSION"
 set -xe
 
 curl --proto '=https' --tlsv1.2 -fSsL https://get-ghcup.haskell.org -o /tmp/get-ghcup.sh
+sed -i "s/edo cabal update --ignore-project/timeout 60s edo cabal update --ignore-project/" /tmp/get-ghcup.sh
 BOOTSTRAP_HASKELL_NONINTERACTIVE=1 \
 BOOTSTRAP_HASKELL_NO_UPGRADE=1 \
 BOOTSTRAP_HASKELL_VERBOSE=1 \
@@ -16,4 +17,4 @@ rm /tmp/get-ghcup.sh
 rm -rf $INSTALLDIR/.ghcup/cache
 rm -rf $INSTALLDIR/.ghcup/share
 rm -rf $INSTALLDIR/.ghcup/ghc/$VERSION/share
-find $INSTALLDIR -name "*.a" | xargs rm
+find $INSTALLDIR -name "*.a" -print0 | xargs -0 rm
